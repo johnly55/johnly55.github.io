ef98c661c1d8e59b8398184d5f7e7526ec82eea7
johnly55 <70291320+johnly55@users.noreply.github.com>
2020-09-23 16:55:22 -0600

    1

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-
-
 function floating(){
   var elem = document.getElementsByClassName("overlay-parent")[0];
   var pos = 0;
-  var id = setInterval(frame, 10);
+  var id = setInterval(frame, 45);
+  var isFalling = false;
   function frame(){
-    if(pos == 350){
-      clearInterval(id);
-    }else{
+    if(pos == 0){
       pos++;
-      elem.style.top = pos + 'px';
-      elem.style.left = pos + 'px';
+      isFalling = false;
+    }else if(!isFalling){
+      pos++;
+      if(pos > 30){
+        isFalling = true;
+      }
+    }else if(isFalling){
+      pos--;
     }
+    elem.style.top = pos + 'px';
   }
 }
+
+floating();
