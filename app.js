1babb3fab7f973f5341148d82a3e42dfcb754307
johnly55 <70291320+johnly55@users.noreply.github.com>
2020-09-23 22:42:43 -0600

    23SEP2020

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function floatingHelmet() {
-  var obj = document.getElementsByClassName('overlay-parent')[0];
+  var obj = document.getElementById('overlay-parent');
   var posY = 0;
   var isFalling = true;
   var range = 25;
@@ -14,46 +14,45 @@ function floatingHelmet() {
     }
     obj.style.top = posY + "px";
     chanceToStartRocket();
-  }, 50);
+  }, 60);
 }
 
 var rocketActive = true;
-function chanceToStartRocket(){
-  if(rocketActive){
+
+function chanceToStartRocket() {
+  if (rocketActive) {
     return;
   }
   var chance = Math.floor(Math.random() * 1000);
-  if(chance > 500){
+  if (chance > 990) {
     rocketActive = true;
     movingRocket();
   }
 }
 
-function movingRocket(){
+function movingRocket() {
   var rocket = new Image();
   rocket.src = "assets/images/rocket.png";
   rocket.style.length = "50px";
   rocket.style.width = "50px";
   rocket.style.position = "absolute";
   rocket.style.transform = "rotate(105deg)"
-  var thing = rocket.style.overlow;
 
-  var posXVariety = Math.floor(Math.random() * 1000);
-  var posX = 2000 + posXVariety;
-  var posY = -200;
-  var intervals = setInterval(function(){
-    posX -= 2;
-    posY += 0.5;
+  var posXVariety = Math.floor(Math.random() * 1000) - 400;
+  var posX = 0 + posXVariety;
+  var posY = -100;
+  var intervals = setInterval(function() {
+    posX += 10;
+    posY += 2.5;
     rocket.style.top = posY + "px";
-    rocket.style.right = posX + "px";
-    if(posX < -100){
+    rocket.style.left = posX + "px";
+    if (posX > 1400 && posY > 300) {
       clearInterval(intervals);
       rocket.parentNode.removeChild(rocket);
       rocketActive = false;
     }
-  }, 5);
-
-  document.getElementsByClassName('rocket-area')[0].appendChild(rocket);
+  }, 30);
+  document.getElementById('rocket-area').appendChild(rocket);
 }
 
 floatingHelmet();
