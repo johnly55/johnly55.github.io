14b2a6395fb7656b28c98a127435269b3dc5d444
johnly55 <70291320+johnly55@users.noreply.github.com>
2020-09-23 21:56:06 -0600

    1

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ function floatingHelmet() {
   var posY = 0;
   var isFalling = true;
   var range = 25;
-  var intervals = setInterval(function() {
+  setInterval(function() {
     if (isFalling == true) {
       posY--;
     } else {
@@ -16,4 +16,31 @@ function floatingHelmet() {
   }, 50);
 }
 
+function chanceToStartRocket()
+
+function movingRocket(){
+  var rocket = new Image();
+  rocket.src = "assets/images/rocket.png";
+  rocket.style.length = "50px";
+  rocket.style.width = "50px";
+  rocket.style.position = "absolute";
+  rocket.style.transform = "rotate(105deg)"
+  var thing = rocket.style.overlow;
+
+  var posX = 2000;
+  var posY = -200;
+  var intervals = setInterval(function(){
+    posX -= 2;
+    posY += 0.5;
+    rocket.style.top = posY + "px";
+    rocket.style.right = posX + "px";
+    if(posX < -100){
+      clearInterval(intervals);
+    }
+  }, 5);
+
+  document.getElementsByClassName('rocket-area')[0].appendChild(rocket);
+}
+
 floatingHelmet();
+movingRocket();
